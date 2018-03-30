32eb05e4d1cf444a201e1b728a1453492129c573
Flairie <37420327+Flairie@users.noreply.github.com>
2018-03-30 16:36:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('ready', () => {
   //client.user.setPresence({ game: { name: 'v2' }});
 });
 
-client.on('message', msg => { /*
+client.on('message', msg => { 
   if(KupotsuPandemy == "1" && !msg.member.roles.exists('id', '423957715614826506') && !msg.member.roles.exists('id', '424064113237295105') && !msg.member.roles.exists('id', '424064220213018625') && !msg.author.bot) {
     msg.channel.sendMessage(msg.member.nickname + ' впал в депрессию!');
     
@@ -30,7 +30,7 @@ client.on('message', msg => { /*
     }
     else msg.member.addRole('424064113237295105');
   }
-  
+  /*
   if(msg.content === '#Cure'){
     msg.reply('Только в последний раз!');
     msg.member.removeRole('424064113237295105');
